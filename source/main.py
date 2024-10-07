6bb730a99f73fcb4ed92d92a0bc772e30ed05887
Carlos563429 <carlitosfg.5051@gmail.com>
2024-10-06 21:58:51 -0600

    [🦈] Feature: ScreenManager support on python file.

--- source/main.py
+++ source/main.py
@@ -1,21 +1,37 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.screen import MDScreen
 from kivy.uix.screenmanager import ScreenManager, Screen
 from modules.sql_connection import Sql
 from modules.exception import LoginError
 from modules.exception import EmptyField
 
-class LoginScreen(MDScreen):
+class LoginScreen(Screen):
     def login(self, username, password):
         print(f"Usuario: {username}, Contraseña: {password}")
 
+    def work(self):
+        print("wtf this work?")
+
+class RegisterDoctorScreen(Screen):
+    def work(self):
+        print("OMG THIS WORK!")
+
 class KioscoApp(MDApp):
     def build(self):
-        return Builder.load_file('login.kv')
+        self.theme_cls.primary_palette = "Olive"
+
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='LoginScreen'))
+        sm.add_widget(RegisterDoctorScreen(name='RegisterDoctorScreen'))
+
+        sm.current = 'LoginScreen'
+
+        Builder.load_file('kiosco.kv')
+
+        #self.screen_manager = Builder.load_file('kiosco.kv')
+        return sm
 
     def login(self, username, password):
-
         try:
             print(Sql.login(username, password))
         except LoginError:
@@ -26,11 +42,8 @@ class KioscoApp(MDApp):
     def work(self):
         print("Yes, this work")
 
-    def nextSlide(self, screen:str):
-        """Change to your screen"""
-        self.root.clear_widgets()
-        otherScreen = Builder.load_file(f'{screen}.kv')
-        self.root.add_widget(otherScreen)
+    def change_screen(self, screen):
+        self.root.current = screen
 
 if __name__ == '__main__':
     KioscoApp().run()
